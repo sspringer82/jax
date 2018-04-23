6c4503fd66cff5242d59278bae93df6c5f1cc3d2
Sebastian Springer <info@sebastian-springer.com>
2018-04-23 10:21:08 +0200

    basic express

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// Prozess bei Änderungen automatisch neu starten
-// npm install -g nodemon
-// Ausführung: nodemon index.js
+// express laden
+const express = require('express');
 
-// Debugger
-// node --inspect-brk index.js
-// node --inspect index.js
-// in den browser wechseln: chrome://inspect
+// app objekt erzeugen
+const app = express();
 
-// https://github.com/npm/node-semver
-
-const http = require('http');
-
-/* don't try this at home
-while (true) {
-  console.log('working');
-}*/
-
-const server = http.createServer((request, response) => {
-  response.end('Hello Fisch');
+app.get('/', (req, res) => {
+  res.json('Hello World');
 });
 
-server.listen(8080, () => {
+// an einen port binden
+app.listen(8080, () => {
   console.log('Server is listening to http://localhost:8080');
 });
